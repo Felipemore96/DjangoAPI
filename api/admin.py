63adeb4f1bbf0902518d70403e5163bdeac5b04c
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-01-21 15:43:57 +0100

    UI first steps

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.contrib.gis.admin import OSMGeoAdmin
+# from .models import GeoFeature
 
-# Register your models here.
+# @admin.register(GeoFeature)
+# class GeoFeatureAdmin(OSMGeoAdmin):
+#     list_display = ('name', 'geometry')  # Display these fields in the admin list view
--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GeoFeatureViewSet
@@ -6,5 +7,9 @@ router = DefaultRouter()
 router.register(r'features', GeoFeatureViewSet, basename='features')
 
 urlpatterns = [
+    # Admin panel route
+    path('admin/', admin.site.urls),
+
+    # API routes
     path('', include(router.urls)),
 ]
